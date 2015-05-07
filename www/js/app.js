e8a53bb1f22b9aedfe48802325163119b1a9f18b
Sebastian Osorio <svzosorio@boolinc.co>
2015-05-06 21:32:08 -0500

    show keyboard accesory

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function(){
-    
+
     angular
         .module('evaluon', ['ionic','evaluon.auth'])
         .run(run)
@@ -18,7 +18,7 @@
           // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
           // for form inputs)
           if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
           }
           if (window.StatusBar) {
             // org.apache.cordova.statusbar required
