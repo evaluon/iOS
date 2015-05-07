4a6720126bac8234eee3c390942dd6b5734f8e07
Sebastian Osorio <svzosorio@boolinc.co>
2015-05-07 06:11:21 -0500

    add user module config

---
+++ www/js/modules/user/module.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function(){
+
+    angular
+        .module('evaluon.user', ['ui.router'])
+        .config(config);
+
+    function config($stateProvider){
+
+        $stateProvider
+
+            .state('home', {
+                url: '/home',
+                templateUrl: 'views/user/home.html',
+                controller: 'HomeController',
+            });
+    };
+
+})();
