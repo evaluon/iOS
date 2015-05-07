08c456efce1e2a206e5fdf789f8c9894cfac55d6
Sebastian Osorio <svzosorio@boolinc.co>
2015-05-07 04:39:05 -0500

    correct the style of code

--- www/js/app.js
+++ www/js/app.js
@@ -9,19 +9,16 @@
 
     function config($stateProvider, $urlRouterProvider){
 
-        // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/login');
     }
 
     function run($ionicPlatform){
         $ionicPlatform.ready(function() {
-          // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-          // for form inputs)
+
           if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
           }
           if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
             StatusBar.styleLightContent();
           }
         });
