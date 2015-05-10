d1ae42a72bb9b5cdf0a88f701ad0e106aafbec3b
Sebastian Osorio <svzosorio@boolinc.co>
2015-05-09 21:33:13 -0500

    create a home controller

---
+++ www/js/modules/user/controllers/home.controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function(){
+
+    angular
+        .module('evaluon.user')
+        .controller('HomeController', HomeController);
+
+    function HomeController(){
+
+    };
+})();
