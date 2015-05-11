b039e60ec794126efaea83f978c3b6b81e965144
Sebastian Osorio <svzosorio@boolinc.co>
2015-05-11 18:55:15 -0500

    change credentials and password object

--- www/js/core/constants.js
+++ www/js/core/constants.js
@@ -20,17 +20,15 @@
         'access', {
             client: {
                 grant_type: 'client_credentials',
-                client_id: 'administrator',
-                client_secret: 'kv0Ls8xoIFPdE2GXMK5fodQsAEBV5GzzINZOA0NX99E='
+                id: 'administrator',
+                secret: 'kv0Ls8xoIFPdE2GXMK5fodQsAEBV5GzzINZOA0NX99E='
             },
             password: function(user, password){
 
                 return {
                     grant_type: 'password',
-                    client_id: this.client.id,
-                    client_secret: this.client.secret,
                     username: user,
-                    password: password
+                    password: CryptoJS.SHA1(password).toString()
                 }
             },
             refresh: function(refresh_token){
