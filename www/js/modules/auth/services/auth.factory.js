0e1dbc702fd2584ec7cd61738b12784ad16dab1a
Sebastian Osorio <svzosorio@boolinc.co>
2015-05-11 18:54:29 -0500

    use client credentials for login

--- www/js/modules/auth/services/auth.factory.js
+++ www/js/modules/auth/services/auth.factory.js
@@ -24,12 +24,16 @@
             },
 
             password: function(username, password){
+                var client = this.client(),
+                            tokenType = client.token_type,
+                            token = client.access_token;
 
                 return $http({
                     method: 'post',
                     url: api.token,
                     headers: {
-                        'Content-Type': headers.urlencoded
+                        'Content-Type': headers.urlencoded,
+                        Authorization: headers.authorization(tokenType, token)
                     },
                     data: $.param(access.password(username, password)),
                 }).then(function(response){
@@ -61,7 +65,7 @@
                 );
             },
 
-            clientLogged: function(){
+            client: function(){
                 return localStorageService.get(
                     CryptoJS.SHA1(tokens.client)
                 );
