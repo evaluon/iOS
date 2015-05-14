fc6c3f644fcf9f3f3c266172743433ca6c2f57e7
Sebastián Osorio <SebastianOsorio@MacBook-Pro-de-Sebastian.local>
2015-05-14 09:16:39 -0500

    send answers

---
+++ www/js/modules/user/services/answer.factory.js
@@ -0,0 +1,65 @@
+(function(){
+    'use strict';
+
+    angular
+    .module('evaluon.user')
+    .factory('Answer' , Answer);
+
+    function Answer($http, Auth, api, headers){
+
+        return{
+            send: function(test, data){
+
+                var user = Auth.userLogged()
+                ,   tokenType = user.token_type
+                ,   token = user.access_token;
+
+                var qs = [];
+
+                for(var i = 0; i < data.length; i++){
+                    qs.push(
+                        (function(d){
+
+                            var sendData = {
+                                test_id: test,
+                                question_id: d.id
+                            };
+
+                            if(d.open){
+                                sendData = _.extend(
+                                    { answer_id: null, text: d.answer }, sendData
+                                );
+                            } else {
+                                sendData = _.extend(
+                                    { answer_id: d.answer }, sendData
+                                    );
+                            }
+
+                            return $http({
+                                method: 'post',
+                                url: api.url + api.response,
+                                headers: {
+                                    Authorization:  headers.authorization(
+                                        tokenType, token
+                                    ),
+                                    'Content-Type': headers.json
+                                },
+                                data: sendData
+                            });
+                        })(data[i])
+                    );
+
+                }
+
+                return $q.all(qs).then(function(responses){
+                    var qSent = _.reduce(responses, function(a, r){ return a + r }, 0);
+                    if(qSent === qs.length){
+                        //console.log("All questions sent");
+                    } else {
+                        //console.log("%d/%d questions sent", qSent, qs.length);
+                    }
+                });
+            }
+        };
+    };
+})();
