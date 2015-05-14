439d9bdf1cafda07af922c3db016549f0a92ed26
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-14 16:58:17 -0500

    add a selfTest states and change controllers of Evaluation states

--- www/js/modules/user/module.js
+++ www/js/modules/user/module.js
@@ -21,22 +21,22 @@
             .state('evaluation', {
                url: '/evaluation',
                templateUrl: 'views/user/evaluation/institutions.html',
-               controller: 'Evaluation.InstitutionsController'
+               controller: 'InstitutionsController'
             })
             .state('evaluation-groups', {
                 url: '/evaluation/institution/:id/groups',
                 templateUrl: 'views/user/evaluation/groups.html',
-                controller: 'Evaluation.GroupsController'
+                controller: 'GroupsController'
             })
             .state('evaluation-tests', {
                 url: '/evaluation/group/:id/tests',
                 templateUrl: 'views/user/evaluation/tests.html',
-                controller: 'Evaluation.TestsController'
+                controller: 'TestsController'
             })
             .state('evaluation-password', {
                 url: '/evaluation/test/:id/password',
                 templateUrl: 'views/user/evaluation/password.html',
-                controller: 'Evaluation.PasswordController'
+                controller: 'PasswordController'
             })
             .state('evaluation-knowledgeAreas', {
                 url: '/evaluation/test/:id/knowledge-areas',
@@ -47,9 +47,29 @@
             .state('evaluation-test', {
                 url: '/evaluation/test/:id/knowledge-area/:area',
                 templateUrl: 'views/user/evaluation/test.html',
-                controller: 'Evaluation.TestController'
+                controller: 'TestController'
             });
 
+
+        //Self Test states
+
+        $stateProvider
+
+            .state('self',{
+                url: '/self/tests',
+                templateUrl: 'views/user/evaluation/tests.html',
+                controller: 'TestsController'
+            })
+            .state('self-knowledgeAreas', {
+                url: '/self/test/:id/knowledge-areas',
+                templateUrl: 'views/user/evaluation/knowledge-areas.html',
+                controller: 'KnowledgeAreas'
+            })
+            .state('self-test', {
+                url: '/self/test/:id/knowledge-area/:area',
+                templateUrl: 'views/user/evaluation/test.html',
+                controller: 'TestController'
+            });
         //Results states
 
         $stateProvider
@@ -65,7 +85,7 @@
                 controller: 'ResultsController'
             });
 
-        //indicators states
+        //Indicators states
 
         $stateProvider
 
