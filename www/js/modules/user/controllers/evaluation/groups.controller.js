269990ce7fceee5797270c35cb910f9598760af2
Sebastián Osorio <SebastianOsorio@MacBook-Pro-de-Sebastian.local>
2015-05-13 23:39:27 -0500

    initial vars and get groups by institution

--- www/js/modules/user/controllers/evaluation/groups.controller.js
+++ www/js/modules/user/controllers/evaluation/groups.controller.js
@@ -1,11 +1,28 @@
 (function(){
 	'use strict';
-	
+
 	angular
 		.module('evaluon.user')
 		.controller('Evaluation.GroupsController', GroupsController);
-		
-	function GroupsController(){
-			
+
+	function GroupsController($scope, $state, Group, list){
+
+		var institution = $state.params.id;
+
+		$scope.groups = [];
+
+		$scope.routes = {
+			tests: 'evaluation-tests'
+		};
+
+		getGroups();
+
+		function getGroups(){
+
+			Group.getList(institution).then(function(data){
+
+				$scope.groups = list.doubled(data);
+			});
+		};
 	};
 })();
