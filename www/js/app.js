13e7119f74d2eb8e957da8f550f312570b888526
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-15 02:48:50 -0500

    add 404, 403 routes and roles

--- www/js/app.js
+++ www/js/app.js
@@ -8,11 +8,30 @@
 
     function config($stateProvider, $urlRouterProvider, $httpProvider){
 
-        $urlRouterProvider.otherwise('/login');
+        $urlRouterProvider.otherwise('/404');
 
         //Interceptor
         $httpProvider.interceptors.push('interceptor');
-    }
+
+        //Error pages
+
+        $stateProvider
+
+            .state('404',{
+                url: '/404',
+                templateUrl: 'views/errors/404.html',
+                data: {
+                    role: 2
+                }
+            })
+            .state('403', {
+                url: '/403',
+                templateUrl: 'views/errors/403.html',
+                data:{
+                    role: 2
+                }
+            });
+    };
 
     function run($ionicPlatform){
         $ionicPlatform.ready(function() {
