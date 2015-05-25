000f824ebf601ba537d7ad951ba981ab59bc0643
Sebastián Osorio <svzosorio@boolinc.co>
2015-05-25 05:33:38 -0500

    update user

--- www/js/modules/user/controllers/config/update.controller.js
+++ www/js/modules/user/controllers/config/update.controller.js
@@ -5,7 +5,29 @@
         .module('evaluon.user')
         .controller('UpdateController', UpdateController);
 
-    function UpdateController(){
+    function UpdateController($scope, evalueeInfo, User){
+
+        var vm = $scope;
+
+        vm.user = {};
+        getUser();
+
+        vm.disabilities = evalueeInfo.disabilities;
+        vm.genders = evalueeInfo.genders;
+        vm.levels = evalueeInfo.levels;
+        vm.types = evalueeInfo.types;
+
+        function getUser(){
+            User.get().then(function(data){
+                console.log(data);
+                data.id = Number(data.id);
+                data.birth_date = new Date(data.birth_date);
+                data.birth_date = data.birth_date.getUTCFullYear() + '' + (data.birth_date.getUTCMonth()+1) + '' + data.birth_date.getUTCDate();
+                data.birth_date = Number(data.birth_date);
+                vm.user = data;
+
+            });
+        };
 
     }
 })()
