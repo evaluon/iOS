fc5c9a23692fc170858f6e1aaf87a57482d3396d
Sebastián Osorio <svzosorio@boolinc.co>
2015-07-08 12:32:08 -0500

    -Bugfix list of institution in results

--- www/js/modules/user/controllers/results/institutions.controller.js
+++ www/js/modules/user/controllers/results/institutions.controller.js
@@ -5,14 +5,19 @@
         .module('evaluon.user')
         .controller('Results.InstitutionsController', InstitutionsController);
 
-    function InstitutionsController($scope, $state, Institution, list){
+    function InstitutionsController($scope, $state,msg, Institution, list){
 
         $scope.institutions = [];
 
         getInstitutions();
 
         function getInstitutions(){
-            Institution.getList().then(function(data){
+            Institution.getResultList().then(function(data){
+                if(data.length == 0){
+                    msg.show('Alerta','No tienes resultados asignados');
+                    $state.go('home');
+                }
+                else
                 $scope.institutions = list.doubled(data);
             });
         };
--- www/js/modules/user/services/institution.factory.js
+++ www/js/modules/user/services/institution.factory.js
@@ -8,7 +8,7 @@
 	function Institution(Auth, $http, api, headers){
 
 		return{
-			
+
 			getList: function(){
 				var user = Auth.userLogged()
 				,	tokenType = user.token_type
@@ -23,7 +23,23 @@
 				}).then(function(response){
 					return response.data.data;
 				});
+			},
+
+			getResultList: function(){
+				var user = Auth.userLogged()
+				,	tokenType = user.token_type
+				,	token = user.access_token;
+
+				return $http({
+					method: 'get',
+					url: api.results,
+					headers:{
+						'Authorization': headers.authorization(tokenType, token)
+					}
+				}).then(function(response){
+					return response.data.data;
+				});
 			}
-		}
-	};
+		};
+	}
 })();
